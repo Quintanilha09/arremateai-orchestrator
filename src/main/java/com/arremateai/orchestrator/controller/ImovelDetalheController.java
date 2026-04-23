a498b8eabe137f43319e5e014f55fdb19c49e4f0
Quintanilha09 <gabriel.hemendinger@gmail.com>
2026-04-23 15:29:10 -0300

    style(E16-H7): corrige violacoes checkstyle pre-existentes

--- src/main/java/com/arremateai/orchestrator/controller/ImovelDetalheController.java
+++ src/main/java/com/arremateai/orchestrator/controller/ImovelDetalheController.java
@@ -4,7 +4,11 @@ import com.arremateai.orchestrator.dto.response.ImovelDetalheResponse;
 import com.arremateai.orchestrator.service.ImovelDetalheService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
--- src/main/java/com/arremateai/orchestrator/controller/VendedorFlowController.java
+++ src/main/java/com/arremateai/orchestrator/controller/VendedorFlowController.java
@@ -5,7 +5,10 @@ import com.arremateai.orchestrator.exception.AcessoNegadoException;
 import com.arremateai.orchestrator.service.VendedorFlowService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
